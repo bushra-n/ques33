78beb3ab4bfa946d29955a1611fc0b4cdaa297da
bushra-n <160867479+bushra-n@users.noreply.github.com>
2024-05-27 09:39:19 +0500

    Add files via upload

---
+++ task33.ts
@@ -0,0 +1,34 @@
+// task 33 ordinal number .....output would be 1st,2nd,3rd
+let numbers:number[] = [1,2,3,4,5,6,7,8,9]
+let number = 3;
+let ordinal:string
+{
+if (number === 1 ){
+    ordinal = number + "st"
+}
+ else if (number === 2){
+    ordinal = number + "nd"
+}
+else if (number === 3){
+    ordinal = number + "rd"
+}
+else 
+    {ordinal = number + "th"
+}
+        console.log(ordinal)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
